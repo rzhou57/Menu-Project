8d52f8dfccfcc3109561fa620f3d1afbd73e4152
Richard Zhou <richardzhou57@gmail.com>
2024-01-28 15:11:52 -0500

    final submit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -42,6 +42,17 @@ const menuItems = [
 ]
 
 function Menu() {
+  const[purchasedItems, setPurchasedItem] = React.useState([])
+
+  const addPurchasedItems = purchasedItem => 
+  {setPurchasedItem([...purchasedItems, purchasedItem])}
+
+  const removePurchasedItems = name =>
+  {setPurchasedItem(purchasedItems.filter(item => name !== item.name))}
+
+  //pass down the function to update the state in the MenuItem component, and pass the state variable as a prop to Receipt, as something 
+  // to be updated
+
   return (
     <section>
       <dl>
@@ -51,11 +62,13 @@ function Menu() {
               name={menuItem.name}
               price={menuItem.price}
               picture={menuItem.picture}
+              addPurchasedItems = {addPurchasedItems}
+              removePurchasedItems = {removePurchasedItems}
             />
           )
         })}
       </dl>
-      <Receipt purchasedItems={[]} />
+      <Receipt purchasedItems={purchasedItems} />
     </section>
   )
 }
